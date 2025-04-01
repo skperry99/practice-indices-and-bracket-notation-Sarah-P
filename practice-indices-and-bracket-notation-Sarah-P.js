0126722be499c2e4cb51180739081e48d816b35f
Sarah <skperry99@users.noreply.github.com>
2025-03-31 20:17:01 -0500

    initial commit

---
+++ practice-indices-and-bracket-notation-Sarah-P.js
@@ -0,0 +1,17 @@
+/*
+Practice: Indices and Bracket
+Notation
+Instructions
+You are managing a library system where books and movies are stored in a 2D
+array, with each row representing a shelf, and each column representing an item
+on that shelf.
+Tasks
+Make sure to write the code for each task using only bracket notation.
+Task 1: Create a multi-dimensional array with nine books and/or movies of your
+choice.
+Task 2: Access and log all the elements in the array using bracket notation with
+numbers.
+Task 3: Access and log all the elements in the array using bracket notation with
+variables as indices. Use the variables row and item.
+Task 4: Write a loop that prints all the items on the second shelf.
+*/
