7a6159cd91acbe4c269da0885f9ed0bb258da159
Sarah <skat1403@gmail.com>
2025-03-31 21:26:40 -0500

    ToDos are done

--- practice-indices-and-bracket-notation-Sarah-P.js
+++ practice-indices-and-bracket-notation-Sarah-P.js
@@ -15,3 +15,27 @@ Task 3: Access and log all the elements in the array using bracket notation with
 variables as indices. Use the variables row and item.
 Task 4: Write a loop that prints all the items on the second shelf.
 */
+
+let myShelf = [["Atomic Habits","Lean In","Think Again"],
+            ["Moana","Tangled","Bolt"],
+            ["Casablanca","The Shining","Juice"]];
+
+console.log(myShelf[0][0]);
+console.log(myShelf[0][1]);
+console.log(myShelf[0][2]);
+console.log(myShelf[1][0]);
+console.log(myShelf[1][1]);
+console.log(myShelf[1][2]);
+console.log(myShelf[2][0]);
+console.log(myShelf[2][1]);
+console.log(myShelf[2][2]);
+
+for(let row=0;row<3;row++){
+    for(let item=0;item<3;item++){
+        console.log(myShelf[row][item]);
+        }
+    }
+
+for (let i = 0; i < myShelf[1].length; i++) {
+    console.log(myShelf[1][i]);
+    }
